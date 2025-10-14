3ea46c04464557a1845c18e7c4ea09ee7ca92286
shariyerShazan <shariyershazan1@gmail.com>
2025-10-14 23:46:10 +0600

    -

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     PrismaModule,
+    AuthModule,
   ],
   controllers: [AppController, AuthController],
   providers: [AppService, PrismaService, AuthService],
