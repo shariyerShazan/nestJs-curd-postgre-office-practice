c76400c1a9d070099d23d29c47882fdc5d1b760d
shariyerShazan <shariyershazan1@gmail.com>
2025-11-07 12:48:12 +0600

    -

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthModule } from './auth/auth.module';
-
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    PrismaModule,
-    AuthModule,
-  ],
-  controllers: [AppController, AuthController],
-  providers: [AppService, PrismaService, AuthService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
